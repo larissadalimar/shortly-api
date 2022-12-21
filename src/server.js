44eca66d52ebc23984f015e9852d4e85633c3a60
larissadalimar <larissadalimar@dcc.ufrj.br>
2022-12-21 20:03:47 -0300

    architecture of project

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config()
+
+const app = express();
+
+app.use(cors);
+app.use(express.json());
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, console.log(`Running server in port ${PORT}`));
